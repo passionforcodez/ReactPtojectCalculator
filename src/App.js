fc00b1ecf35d1a07e8adb747669c9a662cc9a69d
Shwetha <passionforcodez@gmail.com>
2025-02-27 17:50:46 -0500

    React App for calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import * as math from 'mathjs';
 import './App.css';
+import Button from './components/Button';
+import Input from './components/Input';
 
-function App() {
+const App = () => {
+  const [text, setText] = useState("");  // Keep text as string instead of an array
+  const [result, setResult] = useState("");
+
+  const addToText = (val) => {
+    // Append the clicked value to the existing text
+    setText((prevText) => prevText + val);
+  };
+
+  const resetInput = () => {
+    setResult("");
+    setText("");
+  };
+
+  const calResult = () => {
+    try {
+      // Calculate the result of the expression
+      setResult(math.evaluate(text));
+    } catch (error) {
+      setResult("Error");
+    }
+  };
+
+  const buttonColor = "orange";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <h1>Calculator</h1>
+      </div>
+      <div className="App">
+        <div className="calc-wrapper">
+          <Input text={text} result={result} />
+          <div className="row">
+            <Button symbol="7" handleClick={addToText} />
+            <Button symbol="8" handleClick={addToText} />
+            <Button symbol="9" handleClick={addToText} />
+            <Button symbol="/" color={buttonColor} handleClick={addToText} />
+          </div>
+          <div className="row">
+            <Button symbol="4" handleClick={addToText} />
+            <Button symbol="5" handleClick={addToText} />
+            <Button symbol="6" handleClick={addToText} />
+            <Button symbol="*" color={buttonColor} handleClick={addToText} />
+          </div>
+          <div className="row">
+            <Button symbol="1" handleClick={addToText} />
+            <Button symbol="2" handleClick={addToText} />
+            <Button symbol="3" handleClick={addToText} />
+            <Button symbol="+" color={buttonColor} handleClick={addToText} />
+          </div>
+          <div className="row">
+            <Button symbol="0" handleClick={addToText} />
+            <Button symbol="." handleClick={addToText} />
+            <Button symbol="=" handleClick={calResult} />
+            <Button symbol="-" handleClick={addToText} color={buttonColor} />
+          </div>
+          <Button symbol="Clear" handleClick={resetInput} />
+        </div>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
